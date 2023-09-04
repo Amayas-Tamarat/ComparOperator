fb331ddbb8fbfe34ad9c2c8b5eaa519aacba0430
maeldcls <maelduclos03@gmail.com>
2023-09-04 13:56:02 +0200

    some visual for home page

--- html/index.php
+++ html/index.php
@@ -8,21 +8,30 @@ require_once('./connect/connect.php')
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="./css/styles.css">
 </head>
 <body>
     <!----HEADER PART--->
     <div>
-    <nav>
-        <ul>
-            <li><a href="">Accueil</a></li>
-            <li><a href="">Page 2</a></li>
-            <li><a href="">Page 3</a></li>
-            <li><a href="">Page 4</a></li>
-            <li><a href="">Page 5</a></li>
-            <li><a href="">Page 6</a></li>
-        </ul>
-    </nav>
+        <div class="header">
+            <div class="logo">
+                <img src="./assets/picture/logo.jpg" alt="logo bon voyage">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="">Accueil</a></li>
+                    <li><a href="">Page 2</a></li>
+                    <li><a href="">Page 3</a></li>
+                    <li><a href="">Page 4</a></li>
+                    <li><a href="">Page 5</a></li>
+                    <li><a href="">Page 6</a></li>
+                </ul>
+            </nav>
+        </div>
+        
+        <div>
+            <h2>Bon voyage</h2>
+        </div>
     </div>
     <!----HEADER PART--->
     <div>
