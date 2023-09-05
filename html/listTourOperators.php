a6039be1aa706689fe088d268f8d4aa60965a267
maeldcls <maelduclos03@gmail.com>
2023-09-05 13:49:32 +0200

    starting tourOperator page

--- html/listTourOperators.php
+++ html/listTourOperators.php
@@ -2,8 +2,30 @@
 require_once('./connect/autoload.php');
 require_once('./connect/connect.php');
 
-    $manager = new Manager($db);
-    $list = $manager->getAllDestination();
-
-    var_dump($manager->getOperatorByDestination($list[0]));
+$manager = new Manager($db);
+$operator = $manager->findOperatorById(2);
+$tourOperator =$manager->createTourOperator($operator);
+var_dump($tourOperator);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+<?php
+    include('./partials/nav.php');
+    ?>
+    <h3><?php echo $tourOperator->getName() ?></h3>
+
+    <form action="" method="post">
+        <label for=""></label>
+        <input type="number">
+        <label for=""></label>
+        <input type="text">
+    </form>
+</body>
+</html>
