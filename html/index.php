0cc31adf09454ae2f2c75809f04df84836d8f122
Antonio69999 <antoine.chabeauti@gmail.com>
2023-09-07 16:33:05 +0200

    rip

--- html/index.php
+++ html/index.php
@@ -15,7 +15,7 @@ require_once('./connect/connect.php');
     <link rel="stylesheet" href="./css/navbar.css">
     <script src="./js/main.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -23,7 +23,7 @@ require_once('./connect/connect.php');
 
     <div class="accueil">
         <div>
-           
+
         </div>
     </div>
 
