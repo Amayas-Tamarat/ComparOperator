a8332f3397ccd03428db2658b924c77f98613787
Antonio69999 <antoine.chabeauti@gmail.com>
2023-09-08 10:53:51 +0200

    responsive

--- html/index.php
+++ html/index.php
@@ -12,6 +12,7 @@ require_once('./connect/connect.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/navbar.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 
@@ -42,7 +43,7 @@ require_once('./connect/connect.php');
     $destinations = new Manager($db);
     $listeDestinations = $destinations->getAllDestination();
     ?>
-<div class="container">
+<div class="container-fluid">
         <?php foreach ($listeDestinations as $destination) : ?>
             <div class="cards">
                 <a href="./listTourOperators.php?tour_operator_id=<?php echo $destination->getTour_operator_id(); ?>">
